8ee6007be9d73443d60ead1ddaf7e5dc88f4a919
Karim Zanaty <Karim.Zanaty@linrob.io>
2025-08-29 14:46:54 +0000

    add debian files

---
+++ ros2_ws/src/linrob_axis/.obj-x86_64-linux-gnu/rosidl_generator_c/linrob_axis/srv/stop_axis.h
@@ -0,0 +1,12 @@
+// generated from rosidl_generator_c/resource/idl.h.em
+// with input from linrob_axis:srv/StopAxis.idl
+// generated code does not contain a copyright notice
+
+#ifndef LINROB_AXIS__SRV__STOP_AXIS_H_
+#define LINROB_AXIS__SRV__STOP_AXIS_H_
+
+#include "linrob_axis/srv/detail/stop_axis__struct.h"
+#include "linrob_axis/srv/detail/stop_axis__functions.h"
+#include "linrob_axis/srv/detail/stop_axis__type_support.h"
+
+#endif  // LINROB_AXIS__SRV__STOP_AXIS_H_
