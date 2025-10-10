913d23bcb8ff9411afc58d2d92f55f114d02f219
Karim Zanaty <Karim.Zanaty@linrob.io>
2025-10-10 08:45:50 +0000

    New try

---
+++ ros2_ws/src/linrob_axis/debian/ros-humble-linrob-axis/opt/ros/humble/include/linrob_axis/linrob_axis/srv/detail/start_motion__rosidl_typesupport_introspection_c.h
@@ -0,0 +1,47 @@
+// generated from rosidl_typesupport_introspection_c/resource/idl__rosidl_typesupport_introspection_c.h.em
+// with input from linrob_axis:srv/StartMotion.idl
+// generated code does not contain a copyright notice
+
+#ifndef LINROB_AXIS__SRV__DETAIL__START_MOTION__ROSIDL_TYPESUPPORT_INTROSPECTION_C_H_
+#define LINROB_AXIS__SRV__DETAIL__START_MOTION__ROSIDL_TYPESUPPORT_INTROSPECTION_C_H_
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+
+#include "rosidl_runtime_c/message_type_support_struct.h"
+#include "rosidl_typesupport_interface/macros.h"
+#include "linrob_axis/msg/rosidl_typesupport_introspection_c__visibility_control.h"
+
+ROSIDL_TYPESUPPORT_INTROSPECTION_C_PUBLIC_linrob_axis
+const rosidl_message_type_support_t *
+ROSIDL_TYPESUPPORT_INTERFACE__MESSAGE_SYMBOL_NAME(rosidl_typesupport_introspection_c, linrob_axis, srv, StartMotion_Request)();
+
+// already included above
+// #include "rosidl_runtime_c/message_type_support_struct.h"
+// already included above
+// #include "rosidl_typesupport_interface/macros.h"
+// already included above
+// #include "linrob_axis/msg/rosidl_typesupport_introspection_c__visibility_control.h"
+
+ROSIDL_TYPESUPPORT_INTROSPECTION_C_PUBLIC_linrob_axis
+const rosidl_message_type_support_t *
+ROSIDL_TYPESUPPORT_INTERFACE__MESSAGE_SYMBOL_NAME(rosidl_typesupport_introspection_c, linrob_axis, srv, StartMotion_Response)();
+
+#include "rosidl_runtime_c/service_type_support_struct.h"
+// already included above
+// #include "rosidl_typesupport_interface/macros.h"
+// already included above
+// #include "linrob_axis/msg/rosidl_typesupport_introspection_c__visibility_control.h"
+
+ROSIDL_TYPESUPPORT_INTROSPECTION_C_PUBLIC_linrob_axis
+const rosidl_service_type_support_t *
+ROSIDL_TYPESUPPORT_INTERFACE__SERVICE_SYMBOL_NAME(rosidl_typesupport_introspection_c, linrob_axis, srv, StartMotion)();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // LINROB_AXIS__SRV__DETAIL__START_MOTION__ROSIDL_TYPESUPPORT_INTROSPECTION_C_H_
